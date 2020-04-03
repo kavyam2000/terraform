e81a7d8196d221d97c7ce0b063449f098d8c330a
Ubuntu <ubuntu@ip-172-31-16-70.us-east-2.compute.internal>
2020-04-03 20:24:39 +0000

    Add RDS tf files

---
+++ RDS/variables.tf
@@ -0,0 +1,72 @@
+variable "region" {
+  default = "us-east-2"
+}
+
+variable "username" {
+  description = "Master username of the DB"
+  type        = string
+}
+
+variable "password" {
+  description = "Master password of the DB"
+  type        = string
+}
+
+variable "database_name" {
+  description = "Name of the database to be created"
+  type        = string
+}
+
+# ---------------------------------------------------------------------------------------------------------------------
+# OPTIONAL PARAMETERS
+# These parameters have reasonable defaults.
+# ---------------------------------------------------------------------------------------------------------------------
+
+variable "name" {
+  description = "Name of the database"
+  type        = string
+  default     = "terratest-example"
+}
+
+variable "engine_name" {
+  description = "Name of the database engine"
+  type        = string
+  default     = "mysql"
+}
+
+variable "family" {
+  description = "Family of the database"
+  type        = string
+  default     = "mysql5.7"
+}
+
+variable "port" {
+  description = "Port which the database should run on"
+  type        = number
+  default     = 3306
+}
+
+variable "major_engine_version" {
+  description = "MAJOR.MINOR version of the DB engine"
+  type        = string
+  default     = "5.7"
+}
+
+variable "engine_version" {
+  description = "Version of the database to be launched"
+  default     = "5.7.21"
+  type        = string
+}
+
+variable "allocated_storage" {
+  description = "Disk space to be allocated to the DB instance"
+  type        = number
+  default     = 5
+}
+
+variable "license_model" {
+  description = "License model of the DB instance"
+  type        = string
+  default     = "general-public-license"
+}
+
